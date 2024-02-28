2474cbb9526430560dcdeca2bf32e084b2b22395
Syahru Rahmayuda <yudarahma@gmail.com>
2024-02-28 11:22:09 +0700

    Membuat file register.blade php
    menambah register blalde file di resource. view

---
+++ resources/views/register.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Register Form</title>
+</head>
+
+<body>
+    <form>
+        <label>Nama Lengkap</label>
+        <input type="text" class="form-control" name="nama_lengkap">
+    </form>
+</body>
+
+</html>
